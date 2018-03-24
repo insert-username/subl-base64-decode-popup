218903b8475d632d7ff777a19953f22ddc185d29
insert-username <luke.a.dobinson@gmail.com>
2018-03-24 17:11:25 +0000

    Modified plugin to use BaseEncodingUtils to decode and display selected base64 strings.

--- Base64DecodePopupEventListener.py
+++ Base64DecodePopupEventListener.py
@@ -1,6 +1,8 @@
+import sys
 import sublime
 import sublime_plugin
 
+from base64_decode_popup import BaseEncodingUtils
 
 class Base64DecodePopupEventListener(sublime_plugin.EventListener):
 
@@ -8,7 +10,13 @@ class Base64DecodePopupEventListener(sublime_plugin.EventListener):
     def on_selection_modified(self, view):
         selected_text = Base64DecodePopupEventListener.__get_selected_text(view)
 
-        view.show_popup("<p>" + selected_text + "</p>")
+        encoding_analysis = BaseEncodingUtils.analyze(selected_text)
+
+        if encoding_analysis.has_encoding_details():
+            Base64DecodePopupEventListener.__display_encoding_details(
+                encoding_analysis.get_encoding_details(),
+                view)
+
 
 
     @staticmethod
@@ -20,3 +28,21 @@ class Base64DecodePopupEventListener(sublime_plugin.EventListener):
             selected_text += view.substr(region)
 
         return selected_text
+
+
+    @staticmethod
+    def __display_encoding_details(encoding_details, view):
+        encoding_scheme_name = encoding_details.get_encoding_scheme_name()
+        decoded_bytes = encoding_details.get_decoded_bytes()
+
+        decoded_bytes_as_utf8 = \
+            encoding_details.get_decoded_bytes_as_utf8_string()
+
+        encoded_string = encoding_details.get_encoded_string()
+
+        display_string = decoded_bytes_as_utf8 \
+            if decoded_bytes_as_utf8 is not None \
+            else encoding_details.get_decoded_bytes_as_hex_string()
+
+        view.show_popup('<h3>' + encoding_scheme_name + '</h3>' +
+            '<code>' + display_string + '</code>')
