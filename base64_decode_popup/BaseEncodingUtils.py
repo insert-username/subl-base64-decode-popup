1006993679259581a03f20abe637d1465efaa526
insert-username <luke.a.dobinson@gmail.com>
2018-04-02 23:16:10 +0100

    Added EncodedStringFilter.

--- base64_decode_popup/BaseEncodingUtils.py
+++ base64_decode_popup/BaseEncodingUtils.py
@@ -1,4 +1,5 @@
 import sys
+import re
 import base64
 import binascii
 import codecs
@@ -6,6 +7,33 @@ import codecs
 from base64_decode_popup.Base64EncodingAnalyzer import Base64EncodingAnalyzer
 from base64_decode_popup.EncodingAnalysis import EncodingAnalysis
 from base64_decode_popup.Base64UrlEncodingAnalyzer import Base64UrlEncodingAnalyzer
+
+class EncodedStringFilter:
+
+    def __init__(self, \
+        min_encoded_string_length_inclusive=1, \
+        max_encoded_string_length_exclusive=1000, \
+        encoded_string_ignore_filter_regex="^$"):
+
+        if min_encoded_string_length_inclusive < 0:
+            raise ValueError("""min_encoded_string_length_inclusive must be greater than
+                or equal to zero""")
+
+        if max_encoded_string_length_exclusive <= min_encoded_string_length_inclusive:
+            raise ValueError("""max_encoded_string_length_exclusive must be greater than
+                or equal to min_encoded_string_length_inclusive""")
+
+        self.length_range = range( \
+            min_encoded_string_length_inclusive, \
+            max_encoded_string_length_exclusive)
+
+        self.pattern = re.compile(encoded_string_ignore_filter_regex)
+
+
+    def should_evaluate_encoded_string(self, encoded_string):
+        return (len(encoded_string) in self.length_range) and \
+            (not self.pattern.match(encoded_string))
+
 
 def analyze(encoded_string):
     """
---
+++ tests/EncodedStringFilterTest.py
@@ -0,0 +1,57 @@
+import unittest
+
+from base64_decode_popup.BaseEncodingUtils import EncodedStringFilter
+
+class EncodedStringFilterTest(unittest.TestCase):
+
+
+    def test_min_length_may_be_zero(self):
+        EncodedStringFilter(min_encoded_string_length_inclusive=0)
+
+
+    def test_min_length_may_not_be_negative(self):
+        try:
+            EncodedStringFilter(min_encoded_string_length_inclusive=-1)
+            self.fail()
+        except ValueError as e:
+            pass
+
+
+    def test_max_length_exclusive_must_be_greater_than_min_length(self):
+        try:
+            EncodedStringFilter(max_encoded_string_length_exclusive=1)
+            self.fail()
+        except ValueError as e:
+            pass
+
+
+    def test_string_length_filter(self):
+        filter = EncodedStringFilter(min_encoded_string_length_inclusive=10, \
+                                     max_encoded_string_length_exclusive=21)
+
+        self.assertFalse(filter.should_evaluate_encoded_string('a' * 3))
+
+        self.assertFalse(filter.should_evaluate_encoded_string('a' * 9))
+
+        self.assertTrue(filter.should_evaluate_encoded_string('a' * 10))
+
+        self.assertTrue(filter.should_evaluate_encoded_string('a' * 20))
+
+        self.assertFalse(filter.should_evaluate_encoded_string('a' * 21))
+
+
+    def test_string_regex_filter(self):
+        filter = EncodedStringFilter(min_encoded_string_length_inclusive=10, \
+                                     max_encoded_string_length_exclusive=21, \
+                                     encoded_string_ignore_filter_regex="l+")
+
+        self.assertTrue(filter.should_evaluate_encoded_string('a' * 10))
+        self.assertTrue(filter.should_evaluate_encoded_string('a' * 20))
+
+
+        self.assertFalse(filter.should_evaluate_encoded_string('l' * 10))
+        self.assertFalse(filter.should_evaluate_encoded_string('l' * 20))
+
+
+if __name__ == "__main__":
+    unittest.main()
